a29bbd1dff45e8146cffb582587420a25561aa7d
riyao.jin <jinriyao@qq.com>
2025-04-13 14:03:41 +0800

    build_webui在当中设置代理

--- frontend/build-webui.sh
+++ frontend/build-webui.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
+
 . ./setup-nodejs-env.sh
 install_nodejs
+
+# ✅ 设置 npm 代理（必须放在 install_nodejs 后面，因为 nodejs 安装可能替换 PATH）
+npm config set proxy http://127.0.0.1:7890
+npm config set https-proxy http://127.0.0.1:7890
+npm config set registry https://registry.npmjs.org/
+
+# 生成版本号
 package_version=$(head -1 debian/changelog | sed s/.*\(\\\(.*\\\)\).*/\\1/)
 last_commit=$( (git log -1 || echo dev) | head -1 | sed s/commit\ //)
 echo "export const BUILD_VERSION = \"github-$package_version-$last_commit\";" > src/operator/webui/src/environments/version.ts
+
+# 构建前端
 (cd src/operator/webui/ && npm install && ./node_modules/.bin/ng build)
 ok=$?
+
 uninstall_nodejs
 exit $ok
